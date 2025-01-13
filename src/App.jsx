aae789541af367c61659856baeb6aa3a1b547f8b
Alexandre Fernandes <alex.silva250@hotmail.com>
2025-01-12 22:23:22 -0300

    feat: Navbar added to the layout and package updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar"
 import About from "./sections/About"
 import Hero from "./sections/Hero"
 
 function App() {
 
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen
+    w-screen overflow-x-hidden">
+      <Navbar/>
       <Hero/>
       <About/>
     </main>
